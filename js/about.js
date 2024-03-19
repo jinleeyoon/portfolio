245390bc020068ef4c55631b3cf63cb980cce50b
jinleeyoon <yooujinleee@naver.com>
2024-03-19 15:01:24 +0900

    commit1

--- js/about.js
+++ js/about.js
@@ -1,15 +1,26 @@
 let bigPic1 = document.querySelector("#project1>.fullpage");
 let bigPic2 = document.querySelector("#project2>.fullpage");
-let smallPic = document.querySelectorAll(".tab");
+let smallPic1 = document.querySelectorAll("#project1 .tab");
+let smallPic2 = document.querySelectorAll("#project2 .tab");
 
-for (let i = 0; i < smallPic.length; i++) {
-  smallPic[i].addEventListener("click", changePic);
-
+for (let i = 0; i < smallPic1.length; i++) {
+  smallPic1[i].addEventListener("click", changePic);
+ 
+}
+for (let i = 0; i < smallPic2.length; i++) {
+  smallPic2[i].addEventListener("click", changePic1);
+ 
 }
 
 function changePic() {
     let smallPicUrl = window.getComputedStyle(this).backgroundImage;
     smallPicUrl = smallPicUrl.slice(4, -1).replace(/"/g, "");
     bigPic1.style.backgroundImage = `url(${smallPicUrl})`;
+   
+}
+function changePic1() {
+    let smallPicUrl = window.getComputedStyle(this).backgroundImage;
+    smallPicUrl = smallPicUrl.slice(4, -1).replace(/"/g, "");
     bigPic2.style.backgroundImage = `url(${smallPicUrl})`;
+   
 }
